828c94ebc05b5cd32a80d671541415ab8af15d14
Naveen Kumar <naveen.kumar@amway.com>
2024-11-30 13:53:52 +0530

    adding comment to main.yml,acm.tf,dynamodb.tf

--- terrform-infra-code/acm.tf
+++ terrform-infra-code/acm.tf
@@ -4,13 +4,14 @@ provider "aws" {
 }
 
 
+# This resource requests an ACM certificate for your domain
 # Route53 DNS validation for the certificate in us-east-1
 resource "aws_acm_certificate" "cert_useast1" {
   domain_name       = var.domain_name
   validation_method = "DNS"
   provider          = aws.useast1
   lifecycle {
-    create_before_destroy = true
+    create_before_destroy = true #lifecycle: Ensures that the certificate is created before destroying the old one, to avoid downtime.
   }
 
   tags = {
@@ -19,7 +20,10 @@ resource "aws_acm_certificate" "cert_useast1" {
   }
 }
 
-# Route53 DNS validation
+# This resource creates DNS records in Route 53 to validate the ACM certificate.
+# for Expression: Used to create a map or list by iterating over a collection.
+# The for_each meta-argument allows you to create multiple instances of a resource or module based on the items in a map or set. 
+# In your example, for_each is used to create multiple Route53 records:
 resource "aws_route53_record" "cert_validation_useast1" {
   for_each = {
     for dvo in aws_acm_certificate.cert_useast1.domain_validation_options : dvo.domain_name => {
@@ -36,7 +40,7 @@ resource "aws_route53_record" "cert_validation_useast1" {
   records = [each.value.record]
 }
 
-# Verify the certificate validation status in us-east-1
+# This resource waits for the ACM certificate to be validated.
 resource "aws_acm_certificate_validation" "cert_useast1" {
   provider               = aws.useast1
   certificate_arn         = aws_acm_certificate.cert_useast1.arn
--- terrform-infra-code/dynamodb.tf
+++ terrform-infra-code/dynamodb.tf
@@ -1,3 +1,4 @@
+# This resource creates a DynamoDB table.
 resource "aws_dynamodb_table" "my_table" {
   name         = var.dynamodb_table_name  # Name of the DynamoDB table
   billing_mode = "PAY_PER_REQUEST"        # On-demand billing mode
@@ -13,7 +14,8 @@ resource "aws_dynamodb_table" "my_table" {
     Project = "cloud-resume-challenge"
   }
 }
-
+/
+# This resource adds an item to the created DynamoDB table.
 resource "aws_dynamodb_table_item" "example" {
   table_name = aws_dynamodb_table.my_table.name
   hash_key   = aws_dynamodb_table.my_table.hash_key
